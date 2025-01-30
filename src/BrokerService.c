b4b03ca45081498bbc1f93ae252ccbfe4d956d81
michalzd <46342406+michalzd@users.noreply.github.com>
2025-01-30 16:05:26 +0100

    Update BrokerService.c

--- src/BrokerService.c
+++ src/BrokerService.c
@@ -83,7 +83,7 @@ int broker_service_start()
     register int s = 0;
 
     srvsocket = 0;
-    uint16_t servicePort = 0; //było: config.service.port;
+    uint16_t servicePort = config.service.port;
     if(servicePort)
     {
         s = broker_service_socket_open(config.service.port);
